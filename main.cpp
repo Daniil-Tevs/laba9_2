da51cd12c2c954a804eb54b7981739070fb19cc3
Daniil <daniil_tevs@mail.ru>
2022-07-10 15:23:22 +0200

    First commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include <thread>
 #include <chrono>
-#include "function.cpp"
 //Вариант 3.
 //Создайте 3 прямоугольника разных цветов и размеров. Перемещайте их из исходного положения влево
 //до конца экрана с разной скорость. При достижении конца экрана остановите фигуры.
 using namespace std::chrono_literals;
+struct RectangleShape
+{
+    sf::Vector2<float> a;
+    float x{};
+    float y{};
+    float s{};
+    sf::Color c;
+    float x0{};
+    float y0{};
+};
+void PullShape(sf::RectangleShape &shape_z, RectangleShape shape_t)
+{
+    shape_z.setFillColor(shape_t.c);
+    shape_z.setOrigin(shape_t.x0,shape_t.y0);
+}
 
 int main()
 {
@@ -13,7 +27,7 @@ int main()
     RectangleShape shape_a{sf::Vector2f(120.f, 50.f),1000,60,5,sf::Color::Red,60,25};
     RectangleShape shape_b{sf::Vector2f(240.f, 100.f),650,180,3,sf::Color::Yellow,120,50};
     RectangleShape shape_c{sf::Vector2f(70.f, 400.f),1000,600,7, sf::Color::Green,35,300};
-    sf::RectangleShape shape1,shape2(shape_b.a),shape3(shape_c.a);
+    sf::RectangleShape shape1(shape_a.a),shape2(shape_b.a),shape3(shape_c.a);
     PullShape(shape1, shape_a);
     PullShape(shape2, shape_b);
     PullShape(shape3, shape_c);
@@ -25,22 +39,22 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if(shape_a.x-shape_a.s >= 60)
+        if(shape_a.x-shape_a.s >= shape_a.x0)
             shape_a.x -= shape_a.s;
-        else if(shape_a.x != 60)
-            shape_a.x--;
-        if(shape_b.x-shape_b.s >= 120)
+        else if(shape_a.x != shape_a.x0)
+            shape_a.x=shape_a.x0;
+        if(shape_b.x-shape_b.s >= shape_b.x0)
             shape_b.x -= shape_b.s;
-        else if(shape_b.x != 120)
-            shape_b.x--;
-        if(shape_c.x-shape_c.s >= 35)
+        else if(shape_b.x != shape_b.x0)
+            shape_b.x=shape_b.x0;
+        if(shape_c.x-shape_c.s >= shape_c.x0)
             shape_c.x -= shape_c.s;
-        else if(shape_c.x != 35)
-            shape_c.x--;
+        else if(shape_c.x != shape_c.x0)
+            shape_c.x=shape_c.x0;
         shape1.setPosition(shape_a.x,shape_a.y);
         shape2.setPosition(shape_b.x,shape_b.y);
         shape3.setPosition(shape_c.x,shape_c.y);
-        window.clear();
+        window.clear(sf::Color::Blue);
         window.draw(shape1);
         window.draw(shape2);
         window.draw(shape3);
